1287de6caae5b94d8dca6dc200dd6e61d3cd9c71
farelgans <110966748+farelgans@users.noreply.github.com>
2022-08-10 16:23:48 +0800

    Update and rename plugins/donasi.js to plugin/donasi.js

--- plugin/donasi.js
+++ plugin/donasi.js
@@ -1,13 +1,9 @@
 let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 ┏──「 Donasi • Dana 」─⬣
-│ • Pulsa : 085850539404
-│ • Dana : 085850539404
+│ • Pulsa : 082290452317
 ╰────────⬣
 ┏──「 *NOTE* 」─⬣
-│ > Ingin donasi? wa.me/6285850539404
-│ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
-│ _24jam tanpa kendala_
+│ > jika mau donasi seiklasnya saja
 ┗────────⬣
 `.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
 handler.help = ['donasi']
